deac0859e9a6702bd406604b985af28453925c5b
Ian Buckley <ianb@ionconcepts.com>
2023-08-21 19:43:05 -0700

    Introduce ILA in axis_packet_to_stream for bring up debug

--- lib/axis/axis_pkt_to_stream.sv
+++ lib/axis/axis_pkt_to_stream.sv
@@ -76,6 +76,10 @@ module axis_pkt_to_stream
     import drat_protocol::*;
     import axis_pkt_to_stream_pkg::*;
 
+
+    wire [63:0]          probe ; // Debug
+
+
     // Width of FIFO passing unframed IQ samples plus metadata
     localparam C_FIFO_WIDTH = $bits(pkt_to_stream_fifo_t);
 
@@ -186,5 +190,101 @@ module axis_pkt_to_stream
                              .late(late)
                              );
 
+   //-------------------------------------------------------------------------------
+   // Debug Only below
+   //-------------------------------------------------------------------------------
+   //assign probe = 64'h0;
+
+   assign probe[0] =  axis_pkt.tvalid;
+   assign probe[1] = axis_pkt.tready;
+   assign probe[2] =  axis_pkt.tlast;
+   assign probe[10:3] = axis_pkt.tdata[7:0];
+
+   assign probe[11] = axis_status.tvalid;
+   assign probe[12] = axis_status.tready;
+   assign probe[13] = axis_status.tlast;
+   assign probe[21:14] = axis_status.tdata[39:32];
+
+   assign probe[22] = axis_consumption.tvalid;
+   assign probe[23] = axis_consumption.tready;
+   assign probe[24] = axis_consumption.tlast;
+   assign probe[32:25] = axis_consumption.tdata[39:32];
+
+   assign probe[33] = axis_stream.tvalid;
+   assign probe[34] = axis_stream.tready;
+   assign probe[50:35] = axis_stream.tdata[15:0];
+
+   assign probe[51] = run_out;
+
+   assign probe[63:52] = 0;
+
+   ila_64 ila_64_i0 (
+	.clk(clk), // input wire clk
+
+	.probe0(probe[0]), // input wire [0:0]  probe0
+	.probe1(probe[1]), // input wire [0:0]  probe1
+	.probe2(probe[2]), // input wire [0:0]  probe2
+	.probe3(probe[3]), // input wire [0:0]  probe3
+	.probe4(probe[4]), // input wire [0:0]  probe4
+	.probe5(probe[5]), // input wire [0:0]  probe5
+	.probe6(probe[6]), // input wire [0:0]  probe6
+	.probe7(probe[7]), // input wire [0:0]  probe7
+	.probe8(probe[8]), // input wire [0:0]  probe8
+	.probe9(probe[9]), // input wire [0:0]  probe9
+	.probe10(probe[10]), // input wire [0:0]  probe10
+	.probe11(probe[11]), // input wire [0:0]  probe11
+	.probe12(probe[12]), // input wire [0:0]  probe12
+	.probe13(probe[13]), // input wire [0:0]  probe13
+	.probe14(probe[14]), // input wire [0:0]  probe14
+	.probe15(probe[15]), // input wire [0:0]  probe15
+	.probe16(probe[16]), // input wire [0:0]  probe16
+	.probe17(probe[17]), // input wire [0:0]  probe17
+	.probe18(probe[18]), // input wire [0:0]  probe18
+	.probe19(probe[19]), // input wire [0:0]  probe19
+	.probe20(probe[20]), // input wire [0:0]  probe20
+	.probe21(probe[21]), // input wire [0:0]  probe21
+	.probe22(probe[22]), // input wire [0:0]  probe22
+	.probe23(probe[23]), // input wire [0:0]  probe23
+	.probe24(probe[24]), // input wire [0:0]  probe24
+	.probe25(probe[25]), // input wire [0:0]  probe25
+	.probe26(probe[26]), // input wire [0:0]  probe26
+	.probe27(probe[27]), // input wire [0:0]  probe27
+	.probe28(probe[28]), // input wire [0:0]  probe28
+	.probe29(probe[29]), // input wire [0:0]  probe29
+	.probe30(probe[30]), // input wire [0:0]  probe30
+	.probe31(probe[31]), // input wire [0:0]  probe31
+	.probe32(probe[32]), // input wire [0:0]  probe32
+	.probe33(probe[33]), // input wire [0:0]  probe33
+	.probe34(probe[34]), // input wire [0:0]  probe34
+	.probe35(probe[35]), // input wire [0:0]  probe35
+	.probe36(probe[36]), // input wire [0:0]  probe36
+	.probe37(probe[37]), // input wire [0:0]  probe37
+	.probe38(probe[38]), // input wire [0:0]  probe38
+	.probe39(probe[39]), // input wire [0:0]  probe39
+	.probe40(probe[40]), // input wire [0:0]  probe40
+	.probe41(probe[41]), // input wire [0:0]  probe41
+	.probe42(probe[42]), // input wire [0:0]  probe42
+	.probe43(probe[43]), // input wire [0:0]  probe43
+	.probe44(probe[44]), // input wire [0:0]  probe44
+	.probe45(probe[45]), // input wire [0:0]  probe45
+	.probe46(probe[46]), // input wire [0:0]  probe46
+	.probe47(probe[47]), // input wire [0:0]  probe47
+	.probe48(probe[48]), // input wire [0:0]  probe48
+	.probe49(probe[49]), // input wire [0:0]  probe49
+	.probe50(probe[50]), // input wire [0:0]  probe50
+	.probe51(probe[51]), // input wire [0:0]  probe51
+	.probe52(probe[52]), // input wire [0:0]  probe52
+	.probe53(probe[53]), // input wire [0:0]  probe53
+	.probe54(probe[54]), // input wire [0:0]  probe54
+        .probe55(probe[55]), // input wire [0:0]  probe55
+	.probe56(probe[56]), // input wire [0:0]  probe56
+	.probe57(probe[57]), // input wire [0:0]  probe57
+	.probe58(probe[58]), // input wire [0:0]  probe58
+	.probe59(probe[59]), // input wire [0:0]  probe59
+	.probe60(probe[60]), // input wire [0:0]  probe60
+	.probe61(probe[61]), // input wire [0:0]  probe61
+	.probe62(probe[62]), // input wire [0:0]  probe62
+	.probe63(probe[63]) // input wire [0:0]  probe63
+);
 
 endmodule // axis_pkt_to_stream
